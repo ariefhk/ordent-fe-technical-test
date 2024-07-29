bd4d0d65f61a95bda28bd162e13340b4f87ec84c
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2024-07-29 18:24:32 +0700

    chore: change handle divisor by zero by throw an error

--- soal-2.js
+++ soal-2.js
@@ -4,13 +4,13 @@ function remainderOfTwoNum(num1, num2) {
     throw new Error("Invalid type must be a number!");
   }
 
-  // Handle division by zero by returning 0
-  if (num1 === 0 || num2 === 0) {
-    return 0;
-  }
-
   // if num1 is greater than num2, return the remainder of num1 divided by num2
   if (num1 > num2) {
+    // Handle division by zero by throw an error
+    if (num2 === 0) {
+      throw new Error("Division by zero is not allowed!");
+    }
+
     return num1 % num2;
   }
 
